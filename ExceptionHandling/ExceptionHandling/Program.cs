1a17fba8d649ad55c6839aaf85b9cf0f602a994f
LENOVO <john.oduor.j@gmail.com>
2023-07-11 07:22:32 +0300

    Exception handling example

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            // Create a string variable.
+            string str = "Jay";
+
+            try
+            {
+                // Try to access the length of the string.
+                int length = str.Length;
+            }
+            catch (NullReferenceException e)
+            {
+                // Handle the exception.
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                // This code will always execute, regardless of whether or not an exception is thrown.
+                Console.WriteLine("The finally block has executed.");
+            }
+
+            // Exit the program.
+            Console.ReadLine();
+        }
+    }
+}
